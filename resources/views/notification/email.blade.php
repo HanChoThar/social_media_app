f9c620c05c91e45daefa18f1f28f7c577afceb70
hanchothar <henrycho626@gmail.com>
2023-09-03 12:49:14 +0630

    added email blade service

---
+++ resources/views/notification/email.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Our Website</title>
+</head>
+<body>
+    <table style="width: 100%;">
+        <tr>
+            <td align="center">
+                <h1>Welcome to Our Website</h1>
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <p>Dear {{ $userName }},</p>
+                <p>Thank you for joining our website. We're excited to have you on board!</p>
+            </td>
+        </tr>
+        <tr>
+            <td align="center">
+                <p>If you have any questions or need assistance, feel free to contact us.</p>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
